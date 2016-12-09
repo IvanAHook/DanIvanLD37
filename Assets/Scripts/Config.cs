0eb89cceb92a830bd90ff85ac279eabbdd9c42f8
Ivan Höök <Ivan Hk>
2016-12-09 17:12:59 +0100

    config singleton

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -6,25 +6,37 @@ using UnityEngine;
 public class Config : MonoBehaviour
 {
 
+    private static Config _config;
+
+    public Config Instance
+    {
+        get { return _config; }
+    }
+
     private Camera _mainCamera;
 
-	void Awake () {
+    public int Ppu = 32;
+
+	void Awake ()
+	{
+	    if (_config == null)
+	    {
+	        _config = this;
+	    }
+
+	    // CAMERA
         _mainCamera = Camera.main;
 
 	    var verticalResolution = 240;
 	    var horizontalResolution = (verticalResolution / 3) * 4;
 
-	    Debug.Log(horizontalResolution);
-
-	    var ppu = 32;
-
-	    var orthoSize = horizontalResolution / (((horizontalResolution / verticalResolution) * 2) * ppu);
+	    var orthoSize = horizontalResolution / (((horizontalResolution / verticalResolution) * 2) * Ppu);
 
 	    _mainCamera.orthographicSize = orthoSize;
 	    Screen.SetResolution(horizontalResolution, verticalResolution, true);
+	    //
+
+
 	}
-	
-	void Update () {
-		
-	}
+
 }

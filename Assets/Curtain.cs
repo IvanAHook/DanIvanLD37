f398ecf2ece0a2b6ddabba489a3f5fe0c6104e99
dhemgren <dhemgren@kth.se>
2016-12-11 23:47:27 +0100

    curtains

---
+++ Assets/Curtain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtain : MonoBehaviour
+{
+
+    private Animator _animator;
+    private float _counter;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _counter = Random.Range(2f, 4f);
+	    _animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    _counter -= Time.deltaTime;
+
+	    if (_counter <= 0)
+	    {
+	        _counter = Random.Range(4f, 10f);
+	        PlayAnimation();
+	    }
+	}
+
+    private void PlayAnimation()
+    {
+        _animator.speed = Random.Range(0.7f, 1.5f);
+        _animator.Play("Wind", 0);
+    }
+}

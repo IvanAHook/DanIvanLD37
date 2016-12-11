31d715d95d08a90e1c8edbd2f4d8ca2ec526050d
dhemgren <dhemgren@kth.se>
2016-12-11 15:39:28 +0100

    tweak cloth EQ, fire particles, mouse cursor

---
+++ Assets/CursorOnMouseOver.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorOnMouseOver : MonoBehaviour
+{
+
+    public Texture2D toCursor;
+    private Texture2D _prevCursor;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(toCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}

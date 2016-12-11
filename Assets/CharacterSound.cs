2284403a963dd3bf8302d329cda2f6dfed493153
dhemgren <dhemgren@kth.se>
2016-12-11 06:14:10 +0100

    audio

---
+++ Assets/CharacterSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class CharacterSound : MonoBehaviour
+{
+    public AudioClip[] footsteps;
+    private AudioSource _audioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PlayFootStep()
+    {
+        int i = Random.Range(0, 9);
+        _audioSource.pitch = Random.Range(0.8f, 1.2f);
+        _audioSource.PlayOneShot(footsteps[i],1);
+    }
+}
---
+++ Assets/SoundOnMouseOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnMouseOver : MonoBehaviour
+{
+
+    public GameObject nonDiegeticSource;
+    public AudioClip sound;
+    private AudioSource _audioSource;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _audioSource = nonDiegeticSource.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        _audioSource.PlayOneShot(sound,1);
+    }
+}

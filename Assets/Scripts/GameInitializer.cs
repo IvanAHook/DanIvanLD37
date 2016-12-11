02fa2315d8d75119083dff44036535f911bd0b8b
dhemgren <dhemgren@kth.se>
2016-12-11 07:52:33 +0100

    fugly custom cursor (CHANGE!)

--- Assets/Scripts/GameInitializer.cs
+++ Assets/Scripts/GameInitializer.cs
@@ -2,8 +2,12 @@
 
 public class GameInitializer : MonoBehaviour
 {
+    public Texture2D cursor;
+
 	private void Awake()
 	{
 		TurnManager.Initialize();
+	    Cursor.SetCursor(cursor, Vector2.zero, CursorMode.Auto);
+
 	}
 }

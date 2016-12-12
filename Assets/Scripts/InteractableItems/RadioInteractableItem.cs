fe8c2e8161ea093a940219b52d1a7697fb5b8de9
dhemgren <dhemgren@kth.se>
2016-12-12 16:16:06 +0100

    radio sfx on interact

--- Assets/Scripts/InteractableItems/RadioInteractableItem.cs
+++ Assets/Scripts/InteractableItems/RadioInteractableItem.cs
@@ -3,9 +3,48 @@
 public class RadioInteractableItem : InteractableItem
 {
 
+    public AudioClip staticNoVoices;
+    public AudioClip staticVoices;
+    public AudioClip switchOn;
+    public AudioClip switchOff;
+
+    private AudioSource _audioSource;
+    private AudioClip _currentClip;
+    private bool _isActuallyPlaying;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.clip = staticNoVoices;
+    }
+
 	public override void Interract()
 	{
+        if (TurnManager.CurrentDay >= 4)
+	    {
+	        _currentClip = staticVoices;
+	    }
+	    else
+	    {
+	        _currentClip = staticNoVoices;
+	    }
 
+	    _audioSource.clip = _currentClip;
+
+	    if (_isActuallyPlaying)
+	    {
+	        _audioSource.Stop();
+	        _audioSource.PlayOneShot(switchOff, 3f);
+	        _isActuallyPlaying = false;
+	    }
+	    else
+	    {
+	        _audioSource.PlayOneShot(switchOn,3f);
+	        _audioSource.clip = _currentClip;
+	        _audioSource.time = Random.Range(0, _currentClip.length-0.1f);
+	        _audioSource.loop = true;
+	        _audioSource.Play();
+	        _isActuallyPlaying = true;
+	    }
 	}
-
 }

d0ac0fdc17d2fcc40100cd2f6c10f4c9a5d947e9
dhemgren <dhemgren@kth.se>
2016-12-12 13:09:09 +0100

    added birds to bg

---
+++ Assets/Scripts/Birds.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birds : MonoBehaviour
+{
+
+    public Transform start;
+    public Transform stop;
+    public float movementSpeed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.transform.position = start.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    var delta = movementSpeed * Time.deltaTime;
+	    if ((transform.position.x - stop.position.x) < delta)
+	    {
+	        transform.position = start.position;
+	    }
+	    else
+	    {
+	        transform.position = Vector2.MoveTowards(transform.position, stop.position, delta);
+
+	    }
+	}
+}
